b7892b46f5253f260edcc9cafb5ef825c0396d15
root <root@Ty-Laptop.localdomain>
2023-03-04 15:13:26 -0500

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      byebug
+      render json: Student.find_by_first_name(params[:first_name])
+    elsif params[:last_name]
+      render json: Student.find_by(last_name: params[:last_name])
+    else
+     render json: Student.all
+    end
+
   end
+
+  def show
+    render json: Student.find(params[:id])
+  end
+
+
 
 end
